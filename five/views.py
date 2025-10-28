fc412f92a02e170f58f09038dea16e1cde23f1a3
Oluwakanyinsola Wole-Adetayo <kanyinsola@1.local>
2025-10-27 20:00:45 -0500

    MVP: deliver Epic 1 (create/edit/delete/toggle with minimal UI)

--- five/views.py
+++ five/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponseBadRequest
 from .models import Todo
 
 def index(request):
-    todos = Todo.objects.order_by('-created_at') if hasattr(Todo, 'objects') else []
+    todos = Todo.objects.order_by('-created_at')
     return render(request, "five/index.html", {"todos": todos})
 
 
